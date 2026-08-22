8f627f9a0614c3491a30f535ddd74faa5d2cc75d
Cliford M. Dareus <104238549+cliford-dareus@users.noreply.github.com>
2026-08-22 16:58:14 -0400

    style: main layout spacing consistency

--- client/src/components/Layout/styles.ts
+++ client/src/components/Layout/styles.ts
@@ -1,26 +1,27 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  margin-inline: 2%;
-  max-width: 1140px;
+  margin-inline: 4%;
+  max-width: 1200px;
 
-  @media screen and (min-width: 425px) {
+  @media screen and (min-width: 640px) {
+    margin-inline: 6%;
+  }
+
+  @media screen and (min-width: 1024px) {
     margin-inline: 8%;
   }
 
-  @media screen and (min-width: 605px) {
-    margin-inline: 15%
-  }
-
-
   @media screen and (min-width: 1440px) {
     margin-inline: auto;
+    padding-inline: 1em;
   }
 `;
 
 export const MainContainer = styled.div`
   display: flex;
-  gap: 1em;
+  gap: var(--content-gap);
   margin-top: 1em;
-  height: calc(100vh - 60px - 1em);
+  height: calc(100vh - var(--header-height) - 1em);
+  align-items: flex-start;
 `;
