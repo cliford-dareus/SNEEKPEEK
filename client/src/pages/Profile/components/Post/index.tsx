38482eca74e78f4f5659f24b838914ee43298b7e
Cliford M. Dareus <104238549+cliford-dareus@users.noreply.github.com>
2026-08-22 16:11:33 -0400

    fix: profile posts use stable keys and empty state

--- client/src/pages/Profile/components/Post/index.tsx
+++ client/src/pages/Profile/components/Post/index.tsx
@@ -3,22 +3,46 @@ import Card from "../../../../components/UI/Card";
 import { useGetUserPostQuery } from "../../../../features/api/post";
 import Loader from "../../../../components/UI/Loader";
 import { LoaderContainer } from "../../index";
+import { IPost } from "../../../../utils/types/types";
+import styled from "styled-components";
 
 const Index = () => {
   const { name } = useParams();
-  const { data: post, isLoading } = useGetUserPostQuery(name);
+  const { data: post, isLoading, isError } = useGetUserPostQuery(name || "", {
+    skip: !name,
+  });
+
+  if (isLoading) {
+    return (
+      <LoaderContainer>
+        <Loader />
+      </LoaderContainer>
+    );
+  }
+
+  if (isError) {
+    return <Empty>Could not load posts.</Empty>;
+  }
+
+  const posts: IPost[] = post?.post ?? [];
+
+  if (posts.length === 0) {
+    return <Empty>No posts yet.</Empty>;
+  }
 
   return (
     <div>
-      {!isLoading ? (
-        post?.post.map((post: any) => <Card post={post} />)
-      ) : (
-        <LoaderContainer>
-          <Loader />
-        </LoaderContainer>
-      )}
+      {posts.map((p) => (
+        <Card key={p._id} post={p} />
+      ))}
     </div>
   );
 };
 
 export default Index;
+
+const Empty = styled.p`
+  text-align: center;
+  padding: 2em 1em;
+  opacity: 0.7;
+`;
