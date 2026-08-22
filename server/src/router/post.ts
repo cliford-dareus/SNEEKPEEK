b6ecfcce56e50ee3deb8ffbb6dfc7f87c554aabe
Cliford M. Dareus <104238549+cliford-dareus@users.noreply.github.com>
2026-08-22 16:08:42 -0400

    feat: routes for tagged posts and trending

--- server/src/router/post.ts
+++ server/src/router/post.ts
@@ -6,6 +6,8 @@ import {
   editPost,
   getAllPost,
   getPostwithCommment,
+  getTaggedInPosts,
+  getTrendingPosts,
   getUserPost,
   likeOrUnlikePost,
 } from "../controller/post";
@@ -14,7 +16,9 @@ const router = expres.Router();
 
 router.route("/").post(isAuthenticated, createPost).get(getAllPost);
 router.route("/edit").patch(isAuthenticated, editPost);
+router.route("/trending").get(getTrendingPosts);
 router.route("/personal/:username").get(getUserPost);
+router.route("/tagged/:username").get(getTaggedInPosts);
 router
   .route("/:postId")
   .delete(isAuthenticated, deletePost)
