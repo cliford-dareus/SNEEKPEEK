da63c52bed2b78a702c9bcfba35bbf9a2069432f
Cliford M. Dareus <104238549+cliford-dareus@users.noreply.github.com>
2026-08-22 15:55:49 -0400

    security: hide password by default, enforce min length on schema

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -8,23 +8,25 @@ const UserSchema = new Schema<IUser, UserModel, IUserMethod>(
     name: {
       type: String,
       required: true,
-      min: 4,
-      max: 20,
+      minlength: 4,
+      maxlength: 20,
       trim: true,
     },
     username: {
       type: String,
       required: true,
       unique: true,
-      min: 4,
-      max: 20,
+      minlength: 4,
+      maxlength: 20,
       trim: true,
+      lowercase: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
       trim: true,
+      lowercase: true,
       validate: [validator.isEmail, "Invalid email..."],
     },
     image: {
@@ -34,6 +36,8 @@ const UserSchema = new Schema<IUser, UserModel, IUserMethod>(
     password: {
       type: String,
       required: true,
+      minlength: 8,
+      select: false,
     },
     request: [
       {
@@ -61,7 +65,7 @@ const UserSchema = new Schema<IUser, UserModel, IUserMethod>(
 
 UserSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
-  const salt = await bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password!, salt);
 });
 
