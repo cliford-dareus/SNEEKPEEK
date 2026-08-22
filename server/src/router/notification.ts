120e33960111b48a39f9a6a6401c9152b430c409
Cliford M. Dareus <104238549+cliford-dareus@users.noreply.github.com>
2026-08-22 19:23:37 -0400

    fix: notification mark-read routes

--- server/src/router/notification.ts
+++ server/src/router/notification.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import isAuthenticated from "../middleware/isAuthenticated";
-import { getNotification } from "../controller/notification";
+import {
+  getNotification,
+  markAllNotificationsRead,
+  markNotificationRead,
+} from "../controller/notification";
 
 const router = express.Router();
 
 router.route("/").get(isAuthenticated, getNotification);
+router.route("/read-all").patch(isAuthenticated, markAllNotificationsRead);
+router.route("/:id/read").patch(isAuthenticated, markNotificationRead);
 
 export default router;
