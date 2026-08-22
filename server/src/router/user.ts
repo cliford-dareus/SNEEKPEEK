be7118fbf68bcb67f17ea84737ddd27522cd4d0c
Cliford M. Dareus <104238549+cliford-dareus@users.noreply.github.com>
2026-08-22 15:46:04 -0400

    fix: update isAuthenticated import path (remove trailing space)

--- server/src/router/user.ts
+++ server/src/router/user.ts
@@ -8,13 +8,13 @@ import {
   getUserByName,
   searchUser,
 } from "../controller/user";
-import isAuthenticated from "../middleware/isAuthenticated ";
+import isAuthenticated from "../middleware/isAuthenticated";
 
 const router = expres.Router();
 
 router.route("/").get(searchUser);
 router.route("/single/:id").get(getUser);
-router.route('/:username').get(getUserByName)
+router.route("/:username").get(getUserByName);
 router.route("/update").patch(isAuthenticated, editUser);
 router.route("/follow/:username").post(isAuthenticated, followUser);
 router
