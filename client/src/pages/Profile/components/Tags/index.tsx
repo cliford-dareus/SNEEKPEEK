4664641d68a3f442412a3fd6ff80fa007eb37465
Cliford M. Dareus <104238549+cliford-dareus@users.noreply.github.com>
2026-08-22 16:10:18 -0400

    feat: Profile Tags tab lists posts the user is tagged in

--- client/src/pages/Profile/components/Tags/index.tsx
+++ client/src/pages/Profile/components/Tags/index.tsx
@@ -1,8 +1,48 @@
+import { useParams } from "react-router-dom";
+import Card from "../../../../components/UI/Card";
+import Loader from "../../../../components/UI/Loader";
+import { useGetTaggedPostsQuery } from "../../../../features/api/post";
+import { IPost } from "../../../../utils/types/types";
+import { LoaderContainer } from "../../index";
+import styled from "styled-components";
 
 const Index = () => {
-  return (
-    <div>Tags</div>
-  )
-}
+  const { name } = useParams();
+  const { data, isLoading, isError } = useGetTaggedPostsQuery(name || "", {
+    skip: !name,
+  });
 
-export default Index
+  if (isLoading) {
+    return (
+      <LoaderContainer>
+        <Loader />
+      </LoaderContainer>
+    );
+  }
+
+  if (isError) {
+    return <Empty>Could not load tagged posts.</Empty>;
+  }
+
+  const posts: IPost[] = data?.post ?? [];
+
+  if (posts.length === 0) {
+    return <Empty>No tagged posts yet.</Empty>;
+  }
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <Card key={post._id} post={post} />
+      ))}
+    </div>
+  );
+};
+
+export default Index;
+
+const Empty = styled.p`
+  text-align: center;
+  padding: 2em 1em;
+  opacity: 0.7;
+`;
