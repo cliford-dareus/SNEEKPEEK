ae98db7536f8ccef5d91fb977cc6bc700c22ae4c
Cliford M. Dareus <104238549+cliford-dareus@users.noreply.github.com>
2026-08-22 20:52:11 -0400

    feat: mount GlobalSocketListener in main layout

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -1,15 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../UI/Sidebar";
 import Navbar from "../UI/Navbar";
+import GlobalSocketListener from "../GlobalSocketListener";
 import { Container, MainContainer } from "./styles";
 
-
-
 const Index = () => {
-  
-
   return (
     <Container>
+      <GlobalSocketListener />
       <Navbar />
       <MainContainer>
         <Sidebar />
